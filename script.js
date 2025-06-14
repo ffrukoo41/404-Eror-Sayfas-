955af9c37f40f0cb7f700676d06210fe64db592b
ffrukoo41 <furkanenes4178@gmail.com>
2025-06-14 14:06:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const caveman = document.getElementById("caveman");
-    caveman.addEventListener("mouseover", function() {
-        caveman.style.transform = "rotate(10deg)";
-    });
-    caveman.addEventListener("mouseout", function() {
-        caveman.style.transform = "rotate(0deg)";
-    });
-});
+
