a3d93279640513756692d4812d4f0d821d573602
ffrukoo41 <furkanenes4178@gmail.com>
2025-06-14 13:53:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("404 sayfası yüklendi!");
+document.addEventListener("DOMContentLoaded", function() {
+    const caveman = document.getElementById("caveman");
+    caveman.addEventListener("mouseover", function() {
+        caveman.style.transform = "rotate(10deg)";
+    });
+    caveman.addEventListener("mouseout", function() {
+        caveman.style.transform = "rotate(0deg)";
+    });
+});
